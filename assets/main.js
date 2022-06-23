3b7747b2d4c4208ee5bc587f7d609ae05edfd627
speedbuild98 <lautaropectm98@gmail.com>
2022-06-23 14:55:46 -0300

    Re hice todo

---
+++ assets/main.js
@@ -0,0 +1,6 @@
+//Cliente input
+const cliente = document.getElementById('cliente-input');
+//Inicio input
+const inicio = document.getElementById('inicio');
+//Equipo input
+const equipo = document.getElementById('equipo-input');
